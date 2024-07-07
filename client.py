f56875ea759d79e22731bf94283f4a24a06e4928
SaraHammoud <sarahammoud.dev@gmail.com>
2024-07-08 01:17:22 +0300

    new way to connect ip address

--- client.py
+++ client.py
@@ -1,21 +1,51 @@
+# import yaml
+
+# with open('config.yaml', 'r') as file:
+#     config = yaml.safe_load(file)
+
+# run_localhost = config['run_localhost']
+
+# class Client:
+#     def __init__(self):
+#         self.run_localhost = run_localhost
+
+#     def connect(self):
+#         if self.run_localhost:
+#             print("Error: Running on localhost is not allowed.")
+#         else:
+#             print("Connecting to server...")
+
+# if __name__ == "__main__":
+#     client = Client()
+#     client.connect()
+# import yaml
+
+# with open('config.yaml', 'r') as file:
+#     config = yaml.safe_load(file)
+
+# run_localhost = config['run_localhost']
+
+# class Server:
+#     def __init__(self):
+#         self.run_localhost = run_localhost
+
+#     def start(self):
+#         if self.run_localhost:
+#             print("Error: Running on localhost is not allowed.")
+#         else:
+#             print("Server starting...")
+
+# if __name__ == "__main__":
+#     server = Server()
+#     server.start()
 import yaml
+import os
 
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
+config_file = os.path.join(os.path.dirname(__file__), 'C:/Users/user/Desktop/FSW/githubProjects/fullstack-repo/config.yaml')
 
-run_localhost = config['run_localhost']
+with open(config_file, 'r') as f:
+    config = yaml.safe_load(f)
 
-class Client:
-    def __init__(self):
-        self.run_localhost = run_localhost
+server_ip = config.get('ip_address')
 
-    def connect(self):
-        if self.run_localhost:
-            print("Error: Running on localhost is not allowed.")
-        else:
-            print("Connecting to server...")
-
-if __name__ == "__main__":
-    client = Client()
-    client.connect()
-
+print(f"Connecting to server at IP address: {server_ip}")
